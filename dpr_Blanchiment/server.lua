f6f649254b2dfe48c4d902e84a45b36190028350
Desperados <89983547+Desperados-Creation@users.noreply.github.com>
2021-09-24 20:20:32 +0200

    Update server.lua

--- dpr_Blanchiment/server.lua
+++ dpr_Blanchiment/server.lua
@@ -14,22 +14,15 @@ AddEventHandler('dpr_Blanchiment:Blanchir', function(thune)
 
     if thune > 0 and xPlayer.getAccount('black_money').money >= thune then 
         xPlayer.removeAccountMoney('black_money', thune)
-        local xPlayers = ESX.GetPlayers()        
-        for i=1, #xPlayers, 1 do
-            local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-            Citizen.Wait(500)
-			TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Blanchiment', 'Information', "J'te blanchit sa mec attend un peu !", 'CHAR_ARTHUR', 1)
-            Citizen.Wait(10000)
-            TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Blanchiment', 'Information', "Tien mon gars voila t'as thune: "..Total.."$ !", 'CHAR_ARTHUR', 1)
+        Citizen.Wait(500)
+		TriggerClientEvent('esx:showAdvancedNotification', _src, 'Blanchiment', 'Information', "J'te blanchit sa mec attend un peu !", 'CHAR_ARTHUR', 1)
+        Citizen.Wait(10000)
+        TriggerClientEvent('esx:showAdvancedNotification', _src, 'Blanchiment', 'Information', "Tien mon gars voila t'as thune: "..Total.."$ !", 'CHAR_ARTHUR', 1)
         end
 
         xPlayer.addMoney(Total)
     else
-        local xPlayers = ESX.GetPlayers()        
-        for i=1, #xPlayers, 1 do
-            local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-            Citizen.Wait(500)
-			TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Blanchiment', 'Information', "Tu t'fou d'ma gueule ? Du croit tu vas m'arnaquer ?", 'CHAR_ARTHUR', 1)
-        end
+        Citizen.Wait(500)
+		TriggerClientEvent('esx:showAdvancedNotification', _src, 'Blanchiment', 'Information', "Tu t'fou d'ma gueule ? Du croit tu vas m'arnaquer ?", 'CHAR_ARTHUR', 1)
     end
 end)
